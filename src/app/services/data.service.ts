81528a9fc6e309f7f0936f10038b958b10243738
Przemeq25 <przemekcichon97@gmail.com>
2020-06-21 17:10:43 +0200

    lab14-angular done

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -19,6 +19,10 @@ export class DataService {
   getByText(data) {
     return this.http.post(this.url + '/api/posts/', data);
   }
+  createPost(data) {
+    return this.http.post(`${this.url}/posts`, { data: JSON.stringify(data) });
+  }
+
 
 
 }
